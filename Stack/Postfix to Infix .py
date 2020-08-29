020d2bbb0e60ff9343e753dc71ef458d18083257
Arpit Somani <arpit.somanias8@gmail.com>
2020-08-29 14:12:00 +0530

    Postfix to Infix  Transformation

---
+++ Stack/Postfix to Infix .py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Arpit Somani
+"""
+
+
+def isOperand(x): 
+	return ((x >= 'a' and x <= 'z') or
+			(x >= 'A' and x <= 'Z')) 
+
+def getInfix(exp) : 
+
+	s = [] 
+
+	for i in exp:	 
+
+		if (isOperand(i)) :		 
+			s.insert(0, i) 
+
+		else: 
+		
+			op1 = s[0] 
+			s.pop(0) 
+			op2 = s[0] 
+			s.pop(0) 
+			s.insert(0, "(" + op2 + i +
+							op1 + ")") 
+			
+ 
+	return s[0] 
+
+
+if __name__ == '__main__': 
+
+
+	exp=input("Enter your postfix form: ")
+	print("Your infix form is:"  )
+	print(getInfix(exp.strip())) 
+
